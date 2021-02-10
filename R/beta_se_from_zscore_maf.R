6b6001927d3d9b1c10179f79233c4d2d73ce1d4c
rmgpanw <rmgpanw@ucl.ac.uk>
2021-02-10 16:47:02 +0000

    Initial commit

---
+++ R/beta_se_from_zscore_maf.R
@@ -0,0 +1,99 @@
+#' Append columns for beta and standard error values to a dataframe of eQTLGen
+#' cis-eQTL data
+#'
+#' A wrapper around \code{\link{mutate_beta_se_from_zscore}}. Works specifically
+#' for the eQTLGen
+#' \href{https://molgenis26.gcc.rug.nl/downloads/eqtlgen/cis-eqtl/README_cis}{cis-eQTL}
+#' and
+#' \href{https://molgenis26.gcc.rug.nl/downloads/eqtlgen/cis-eqtl/README_allele_frequency}{cis_eQTL
+#' mean allele frequency} datasets.
+#'
+#' @param eqtlgen_cis_eqtl_df Dataframe of eQTLGen cis-eQTL data
+#' @param eqtlgen_cis_eqtl_maf_df eQTLGen cis-eQTL mean allele frequency data
+#'
+#' @return A dataframe with appended \code{beta} and \code{se} columns
+#' @export
+#' 
+mutate_beta_se_from_zscore_eqtlgen_cis_eqtl <- function(eqtlgen_cis_eqtl_df,
+                                                        eqtlgen_cis_eqtl_maf_df) {
+  eqtlgen_cis_eqtl_df %>%
+    dplyr::left_join(eqtlgen_cis_eqtl_maf_df, by = "SNP") %>% 
+    mutate_beta_se_from_zscore(zscore = "Zscore",
+                               maf = "AlleleB_all",
+                               sample_n = "NrSamples",
+                               beta_se = "beta")
+}
+
+
+#' Append columns for beta and standard error values to a dataframe
+#'
+#' Appends columns with beta and standard error values to a dataframe containing
+#' columns for Z-scores, allele frequencies and sample sizes.
+#'
+#' @inheritParams beta_se_from_zscore 
+#'
+#' @return A dataframe with appended \code{beta} and \code{se} columns
+#' @export
+#'
+#' @seealso \code{\link{beta_se_from_zscore}}
+mutate_beta_se_from_zscore <- function(df,
+                                       zscore,
+                                       maf,
+                                       sample_n,
+                                       beta_se) {
+  # mutate beta column
+  df$beta <- beta_se_from_zscore(
+    df = df,
+    zscore = zscore,
+    maf = maf,
+    sample_n = sample_n,
+    beta_se = "beta"
+  )
+  
+  # mutate se column
+  df$se <- beta_se_from_zscore(
+    df = df,
+    zscore = zscore,
+    maf = maf,
+    sample_n = sample_n,
+    beta_se = "se"
+  )
+  
+  return(df)
+}
+
+#' Get beta or standard error from z-score and allele frequency
+#'
+#' Take a dataframe containing Z-scores, allele frequencies and sample sizes and
+#' returns a vector of either beta or standard error values (specified by the
+#' `beta_se` argument)
+#'
+#' @param df A dataframe containing the required input values
+#' @param zscore Colname from \code{df} containing zscores
+#' @param maf Colname from \code{df} containing allele frequencies
+#' @param sample_n Colname from \code{df} containing sample size
+#' @param beta_se Character. Must be either \code{"beta"} or \code{"se"}
+#' 
+#' @return A numerical vector of either beta or standard error values
+#' @export
+
+beta_se_from_zscore <- function(df,
+                                zscore,
+                                maf,
+                                sample_n,
+                                beta_se) {
+    # return beta
+  if (beta_se == 'beta') {
+    df[[zscore]] /
+      sqrt(2 * df[[maf]] * (1 - df[[maf]]) * (df[[sample_n]] + df[[zscore]] ^ 2))
+    
+    # return se
+  } else if (beta_se == "se") {
+    1 / sqrt(2 * df[[maf]] * (1 - df[[maf]]) * (df[[sample_n]] + df[[zscore]] ^ 2))
+    
+    # error if invalid beta_se value
+  } else {
+    stop("Argument 'beta_se' must be either 'beta' or 'se'")
+  }
+  
+}
---
+++ R/process_file_chunked.R
@@ -0,0 +1,152 @@
+#' Append columns for beta and standard error values to the eQTLGen cis-eQTL
+#' dataset.
+#'
+#' Calculates beta and standard error values for the eQTLGen eQTLGen
+#' \href{https://molgenis26.gcc.rug.nl/downloads/eqtlgen/cis-eqtl/README_cis}{cis-eQTL}
+#' dataset from Z-score, sample size and allele frequency (these values are in a
+#' \href{https://molgenis26.gcc.rug.nl/downloads/eqtlgen/cis-eqtl/README_allele_frequency}{
+#' separate file}) values.
+#'
+#' @param eqtlgen_cis_eqtl_filepath Character. Path to
+#' @param eqtlgen_cis_eqtl_maf_filepath Character. Path
+#'   to\href{https://molgenis26.gcc.rug.nl/downloads/eqtlgen/cis-eqtl/README_cis}{cis-eQTL
+#'   file}
+#'   \href{https://molgenis26.gcc.rug.nl/downloads/eqtlgen/cis-eqtl/README_allele_frequency}{cis_eQTL
+#'    mean allele frequency file}
+#' @inheritParams process_file_chunked
+#'
+#' @export
+#' @seealso \code{\link{process_file_chunked}},
+#'   \code{\link{mutate_beta_se_from_zscore_eqtlgen_cis_eqtl}}
+append_beta_se_to_eqtlgen_cis_eqtl <- function(eqtlgen_cis_eqtl_filepath,
+                                               eqtlgen_cis_eqtl_maf_filepath,
+                                               outfile_path,
+                                               chunk_size = 10000) {
+  # read required cols from allele frequency file
+  message("Reading allele frequency file")
+  allele_freq <- data.table::fread(file = eqtlgen_cis_eqtl_maf_filepath,
+                       select = c("AlleleB_all", "SNP"))
+  
+  # process file in chunks
+  process_file_chunked(
+    infile_path = eqtlgen_cis_eqtl_filepath,
+    outfile_path = outfile_path,
+    data_processing_function = mutate_beta_se_from_zscore_eqtlgen_cis_eqtl,
+    chunk_size = chunk_size,
+    eqtlgen_cis_eqtl_maf_df = allele_freq
+  )
+}
+
+#' Apply a processing function to a file and output the result to a new file in
+#' chunks
+#'
+#' A wrapper around \code{\link[readr]{read_delim_chunked}} and
+#' \code{\link[read]{write_delim}}. Allows a large file of tabular data to be
+#' processed in chunks that fit into memory.
+#'
+#' @param infile_path Character. Path to infile.
+#' @param outfile_path Character. Path to outfile. An error is raised if a file
+#'   already exists at this location.
+#' @param data_processing_function Function. A function that accepts a dataframe
+#'   as its first argument and returns a (processed) dataframe also
+#' @inheritParams readr::write_delim
+#' @param ... Additional parameters are passed on to
+#'   \code{data_processing_function}
+#'
+#' @return
+#' @export
+process_file_chunked <- function(infile_path,
+                                 outfile_path,
+                                 data_processing_function,
+                                 chunk_size = 10000,
+                                 delim = "\t",
+                                 col_types = NULL,
+                                 # indexes = NULL,
+                                 verbose = TRUE,
+                                 append = FALSE,
+                                 quote = "\"",
+                                 escape_backslash = FALSE,
+                                 escape_double = TRUE,
+                                 col_names = TRUE,
+                                 locale = readr::default_locale(),
+                                 na = c("", "NA"),
+                                 quoted_na = TRUE,
+                                 comment = "",
+                                 trim_ws = FALSE,
+                                 skip = 0,
+                                 guess_max = min(1000, chunk_size),
+                                 progress = readr::show_progress(),
+                                 skip_empty_rows = TRUE,
+                                 ...) {
+  
+  start_time <- proc.time()
+  
+  # Error message if file already exists
+  if (
+    file.exists(outfile_path)) {
+    stop("Error! ", outfile_path, " already exists.")
+  }
+  
+  # Chunk processing function - applies data_processing_function then writes to outfile
+  f <- function(x, 
+                pos) {
+    time_taken <- proc.time() - start_time
+    
+    if (verbose == TRUE) {
+      message(
+        "Writing from line ",
+        pos,
+        ". Time taken: ",
+        (time_taken[3] %/% 60),
+        " minutes, ",
+        (round(time_taken[3] %% 60)),
+        " seconds"
+      )
+    }
+    
+    x <- data_processing_function(x, ...)
+    
+    # write to outfile
+    readr::write_delim(
+      x = x,
+      file = outfile_path,
+      delim = delim,
+      na = "NA",
+      append = TRUE,
+      col_names = ifelse(pos == 1, TRUE, FALSE),
+      quote_escape = "double",
+      eol = "\n",
+    )
+  }
+  
+  # Read file and write to table in chunks
+  message("Writing chunk to ", outfile_path)
+  readr::read_delim_chunked(
+    file = infile_path,
+    callback = DataFrameCallback$new(f),
+    chunk_size = chunk_size,
+    delim = delim,
+    col_types = col_types,
+    quote = quote,
+    escape_backslash = escape_backslash,
+    escape_double = escape_double,
+    col_names = col_names,
+    locale = locale,
+    na = na,
+    quoted_na = quoted_na,
+    comment = comment,
+    trim_ws = trim_ws,
+    skip = skip,
+    guess_max = guess_max,
+    progress = progress,
+    skip_empty_rows = skip_empty_rows
+  )
+  
+  # Completion message
+  time_taken <- proc.time() - start_time
+  message("Complete. Time taken: ",
+          (time_taken[3] %/% 60),
+          " minutes, ",
+          (round(time_taken[3] %% 60)),
+          " seconds")
+}
